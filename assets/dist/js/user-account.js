5c890874b95c67317c6347954024ec8786d40b39
Viktoriia <toryvna1993@gmail.com>
2019-07-16 14:11:01 +0300

    adding user acc pages

--- assets/dist/js/user-account.js
+++ assets/dist/js/user-account.js
@@ -11,11 +11,22 @@ $(document).ready(function(){
     $('.tooltip').on('click', function(ev){
         $(this).toggleClass('active');
         $(this).parent().parent().find('.tooltip_hidden').toggleClass('active');
-        // if (screen.width > 1023 ) {
-        //     var x = ev.clientX ;
-        //     $(".tooltip_hidden").css('left', x - 30);
-        // }
     });
+
+    $(document).mouseup(function(e){
+        var el = $('.tooltip_hidden.active');
+        if (!el.is(e.target) && el.has(e.target).length === 0) {
+            el.removeClass('active');
+            $('.tooltip').removeClass('active');
+        }
+    });
+
+    $('.toggle_heading .arrow').on('click', function(){
+        $(this).parent().toggleClass('show');
+        $(this).parent().parent().find('.toggle_body').toggleClass('show');
+    })
+
+
     $('.close-tooltip').on('click', function(){
         $(this).parent().removeClass('active');
         $(this).parent().parent().find('.tooltip').removeClass('active');
